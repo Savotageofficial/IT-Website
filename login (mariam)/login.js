ba94e026e0aa5ded0cf51c1ab47157dff779a39b
Savotageofficial <114155950+Savotageofficial@users.noreply.github.com>
2025-05-12 15:43:43 +0300

    Add files via upload

---
+++ login (mariam)/login.js
@@ -0,0 +1,13 @@
+let dark = document.getElementById("log");
+let helwan=document.getElementById("helwan");
+function dark_mode(){
+    dark.classList.toggle("darkl");
+    helwan.classList.toggle("helwan");
+}
+let en  = document.getElementById("en").classList;
+let ar  = document.getElementById("ar").classList;
+function hide(){
+    en.toggle("hiddenen");
+    ar.toggle("hiddenar");
+
+}

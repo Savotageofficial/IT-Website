003d08618053dca38e77d60c2de62e3e3d17e349
Savotageofficial <114155950+Savotageofficial@users.noreply.github.com>
2025-05-12 13:46:02 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,10 @@
+        function toggleDarkMode() {
+         document.body.classList.toggle("dark-mode");
+                  }
+                  let en  = document.getElementById("en").classList;
+      let ar  = document.getElementById("ar").classList;
+      function hide(){
+          en.toggle("hiddenen");
+          ar.toggle("hiddenar");
+
+      }
